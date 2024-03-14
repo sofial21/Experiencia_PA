50e9e684d6121bce0a97ee584ff1995340518a82
sofial21 <141957579+sofial21@users.noreply.github.com>
2024-03-14 13:22:57 +0000

    listo

--- juegos/adivinar_numero.py
+++ juegos/adivinar_numero.py
@@ -1,7 +1,10 @@
+import random
+
 def adivinar_numero():
     """
     Esta función representa el juego de adivinar un número.
     Debes generar un número al azar entre 1 y 10, y luego pedir al usuario que adivine el número.
     Se debe mostrar un mensaje si el usuario adivina correctamente o no.
     """
+
     pass
