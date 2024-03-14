beb572d3fe5181caf416a29ffba0834df2ba351c
Quezada08 <141894207+Quezada08@users.noreply.github.com>
2024-03-14 17:22:34 +0000

    subir memoria

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,3 +1,4 @@
+import random
 def memoria():
     """
     Esta función representa el juego de memoria.
@@ -5,4 +6,16 @@ def memoria():
     Luego, debes pedir al usuario que repita la secuencia.
     Se debe mostrar un mensaje si el usuario acierta o no.
     """
-    pass
+    secuencia = []
+    for i in range(10):
+        numero = random.randint(0,101)
+        secuencia = secuencia.append(numero) 
+        print(numero)
+    
+    for i in secuencia:
+        ingresar = input('Ingresa un número: ')
+        if ingresar != i:
+            return("Fallaste")
+    
+    return("Ganaste")
+            
