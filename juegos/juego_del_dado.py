e2838c3b058d50a0ac98dc0cc49a51dffd8ba917
sofial21 <141957579+sofial21@users.noreply.github.com>
2024-03-14 18:58:57 +0000

    aaa

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -21,25 +21,23 @@ def juego_del_dado():
         print("Puntaje jugador: " + str(puntuacion_usuario))
         print("Puntaje computador: " + str(puntuacion_computador))
         print("Juego continua ... ")
-
-        if puntuacion_usuario >= 30 and puntuacion_computador >= 30:
+        
+        if puntuacion_usuario >= 30:
             print("Puntaje jugador" + str(lanzamiento))
             print("Puntaje computador" + str(lanzamiento_computador))            
             juego = False
             print("Juego terminado ... ")
-            print('Empate!')
-        
-        else:
-            if puntuacion_usuario >= 30:
-                print("Puntaje jugador" + str(lanzamiento))
-                print("Puntaje computador" + str(lanzamiento_computador))            
-                juego = False
-                print("Juego terminado ... ")
+            if puntuacion_usuario >= 30 and puntuacion_computador >= 30:
+                print('Empate!')
+            else:
                 print('Ganaste!')
 
-            if puntuacion_computador >= 30:
-                print("Puntaje jugador" + str(lanzamiento))
-                print("Puntaje computador" + str(lanzamiento_computador))
-                juego = False
-                print("Juego terminado ... ")
+        if puntuacion_computador >= 30:
+            print("Puntaje jugador" + str(lanzamiento))
+            print("Puntaje computador" + str(lanzamiento_computador))
+            juego = False
+            print("Juego terminado ... ")
+            if puntuacion_usuario >= 30 and puntuacion_computador >= 30:
+                print('Empate!')
+            else:
                 print('Perdiste :(')
